e8823a4120833914167f789b5a863f529ece7fa8
adarean5 <strazisar.jernej@gmail.com>
2018-12-20 16:58:31 +0100

    A few changes to the card structure

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -1,12 +1,26 @@
 <template>
-    
+    <div class="orderCard">
+        <div>
+            <p></p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Card"
+    name: "Card",
+    data() {
+        return {
+            arrivalTime: 0,
+            ownerName: "",
+            priceTotal: 0,
+            totalPrepTime: 0
+        };
+    },
+    mounted() {
+
+    }
 };
 </script>
-
 <style scoped>
 </style>
