de3ad88abecea8b5af4f1f2bb80f6d438675ce08
Simon Korošec <simon.korosec1997@gmail.com>
2019-01-12 12:55:08 +0100

    test cors fix

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const cors = require('cors');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
 
+app.use(cors());
+
+app.use(serveStatic(__dirname + "/dist"));
 app.use('/dist', express.static('dist'));
 
 const port = process.env.PORT || 5000;
